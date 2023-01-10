4a0dc43e65cd92b4aae95b9ce47eec8b83938f17
JainPrat <122390803+JainPrat@users.noreply.github.com>
2023-01-11 00:14:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 
 module "resourcegroup" {
   source         = "./modules/resourcegroup"
-  name           = var.name
-  location       = var.location
+  name           = "rgtest"
+  location       = "northeurope"
 }
 
 module "vnet" {
   source              = "./modules/vnet"
+  name                = "vmtest"
   location            = module.resourcegroup.location_id
   resource_group      = module.resourcegroup.resource_group_name
   address_space       = var.vnet
@@ -16,7 +17,7 @@ module "websubnet" {
   source         = "./modules/subnet"
   location       = module.resourcegroup.location_id
   resource_group = module.resourcegroup.resource_group_name
-  vnetname       = var.vnet_name
+  vnetname       = module.vnet.vnet_name
   address_prefixes  = var.websubnet
 }
 
@@ -24,7 +25,7 @@ module "appsubnet" {
   source         = "./modules/subnet"
   location       = module.resourcegroup.location_id
   resource_group = module.resourcegroup.resource_group_name
-  vnetname       = var.vnet_name
+  vnetname       = module.vnet.vnet_name
   address_prefixes  = var.appsubnet
 }
 
@@ -32,49 +33,38 @@ module "dbsubnet" {
   source         = "./modules/subnet"
   location       = module.resourcegroup.location_id
   resource_group = module.resourcegroup.resource_group_name
-  vnetname       = var.vnet_name
+  vnetname       = module.vnet.vnet_name
   address_prefixes   = var.dbsubnet
 }
 
-module "webnsg" {
+module "nsg" {
   source         = "./modules/nsg"
   location       = module.resourcegroup.location_id
   resource_group = module.resourcegroup.resource_group_name 
-  web_subnet_id  = module.subnet.websubnet_id
+  web_subnet_id  = module.websubnet.subnet_id
+  app_subnet_id  = module.appsubnet.subnet_id
+  db_subnet_id   = module.dbsubnet.subnet_id
 }
 
-module "appnsg" {
-  source         = "./modules/nsg"
-  location       = module.resourcegroup.location_id
-  resource_group = module.resourcegroup.resource_group_name 
-  app_subnet_id  = module.subnet.appsubnet_id
-}
-
-module "dbnsg" {
-  source         = "./modules/nsg"
-  location       = module.resourcegroup.location_id
-  resource_group = module.resourcegroup.resource_group_name 
-  db_subnet_id   = module.subnet.dbsubnet_id
-}
 
 module "webvm" {
   source         = "./modules/vm"
   location = module.resourcegroup.location_id
   resource_group = module.resourcegroup.resource_group_name
-  web_subnet_id = module.websubnet.websubnet_id
-  web_host_name = var.web_host_name
-  web_username = var.web_username
-  web_password = var.web_password
+  subnet_id = module.websubnet.websubnet_id
+  host_name = var.web_host_name
+  username = var.web_username
+  password = var.web_password
 }
 
 module "appvm" {
   source         = "./modules/vm"
   location = module.resourcegroup.location_id
   resource_group = module.resourcegroup.resource_group_name
-  app_subnet_id = module.appsubnet.appsubnet_id
-  app_host_name = var.app_host_name
-  app_username = var.app_username
-  app_password = var.app_password
+  subnet_id = module.appsubnet.appsubnet_id
+  host_name = var.app_host_name
+  username = var.app_username
+  password = var.app_password
 }
 
 module "database" {
@@ -83,7 +73,6 @@ module "database" {
   resource_group = module.resourcegroup.resource_group_name
   sqlserver_name =var.server_name
   database_name = var.database_name
-  database_version = var.database_version
   database_admin = var.database_admin
   database_password = var.database_password
 }
