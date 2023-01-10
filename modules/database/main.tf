257a160e2674c728e9cb27a9778659a4929647ab
JainPrat <122390803+JainPrat@users.noreply.github.com>
2023-01-11 00:13:13 +0530

    Update main.tf

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -1,16 +1,16 @@
 resource "azurerm_sql_server" "test" {
-    name = var.primary_database
+    name = var.server_name
     resource_group_name = var.resource_group
     location = var.location
-    version = var.primary_database_version
-    administrator_login = var.primary_database_admin
-    administrator_login_password = var.primary_database_password
+    version  = "12.0"
+    administrator_login = var.database_admin
+    administrator_login_password = var.database_password
 }
 
 resource "azurerm_storage_account" "example" {
   name                     = "examplesa"
-  resource_group_name      = ""
-  location                 = ""
+  resource_group_name      = var.resource_group
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
@@ -19,5 +19,5 @@ resource "azurerm_sql_database" "testdb" {
   name                = "db"
   resource_group_name = var.resource_group
   location            = var.location
-  server_name         = azurerm_sql_server.primary.name
+  server_name         = azurerm_sql_server.test.name
 }
