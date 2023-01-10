657b62934d50604aee8118ca6e2a91fe797ce10f
JainPrat <122390803+JainPrat@users.noreply.github.com>
2023-01-11 00:15:55 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,48 +1,57 @@
-variable "name" {
+variable "vnet" {
+ type=string
+ default = "10.0.0.0/16"
+}
+variable "websubnet" {
   type=string
+  default = "10.0.1.0/24"
 }
-variable "location" {
+variable "appsubnet" {
  type=string
+ default = "10.0.2.0/24"
 }
-variable "vnetcidr" {
+variable "dbsubnet" {
  type=string
-}
-variable "websubnetcidr" {
-  type=string
-}
-variable "appsubnetcidr" {
- type=string
-}
-variable "dbsubnetcidr" {
- type=string
+ default = "10.0.3.0/24"
 }
 variable "web_host_name"{
  type=string
+ default = "webhost"
 }
 variable "web_username" {
   type=string
+  default = "user1"
 }
-variable "web_os_password" {
+variable "web_password" {
  type=string
+ default = "pass1"
 }
 variable "app_host_name"{
  type=string
+ default = "apphost"
 }
 variable "app_username" {
  type=string
+ default = "user2"
 }
-variable "app_os_password" {
+variable "app_password" {
  type=string
+ default = "pass2"
 }
-variable "primary_database" {
+variable "database_name" {
  type=string
+ default = "db1"
 }
-variable "primary_database_admin" {
+variable "database_admin" {
  type=string
+ default = "localadmin"
 }
-variable "primary_database_password" {
+variable "database_password" {
  type=string
+ default = "localpass"
 }
-variable "primary_database_version" {
+
+variable "server_name" {
  type=string
+ default = "mssql"
 }
