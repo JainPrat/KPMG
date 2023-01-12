03c92777453904a9992e655820bb4999eed2385f
JainPrat <122390803+JainPrat@users.noreply.github.com>
2023-01-12 07:21:37 +0530

    Create variable.tf

---
+++ modules/vm/variable.tf
@@ -0,0 +1,19 @@
+variable "resource_group" {
+ type = string
+ default = "testrg"
+}
+
+variable "location" {
+ type = string
+ default = "northeurope"
+}
+
+variable "admin_username" {
+ type = string
+ default = "user1"
+}
+
+variable "admin_password" {
+ type = string
+ default = "password"
+}
