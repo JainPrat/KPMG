a8e2ed7075654d76a36a09cc5bc8097dd6a6d35a
JainPrat <122390803+JainPrat@users.noreply.github.com>
2023-01-12 07:16:13 +0530

    Update variable.tf

--- modules/nsg/variable.tf
+++ modules/nsg/variable.tf
@@ -1,3 +1,13 @@
-variable web_subnet_id {
+variable "web_subnet_id" {
  type String
+}
+
+variable "resource_group" {
+ type String
+ default = "testrg"
+}
+
+variable "location" {
+ type String
+ default = "northeurope"
 }
